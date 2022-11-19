e3f932791af4dc79df500c26607cdfa99ef3d126
Erick B. Lemos <ericklemoscosta@gmail.com>
2022-11-19 17:44:18 -0300

    habilitar e desabilitar plugin

--- src/main/kotlin/org/cabradati/lulaupdate/App.kt
+++ src/main/kotlin/org/cabradati/lulaupdate/App.kt
@@ -7,12 +7,16 @@ import org.cabradati.lulaupdate.events.LulaSpawnEvent
 class App: JavaPlugin() {
 
     companion object {
+        const val ATIVAR_PLUGIN = "plugin.enabled"
+        const val ATIVAR_DEBUG = "plugin.debug"
         const val VIDA = "config.health"
         const val QUANTIDADE_DE_PEIXES = "config.drop_quantity"
     }
 
     override fun onEnable() {
 
+        config.addDefault(ATIVAR_PLUGIN, true)
+        config.addDefault(ATIVAR_DEBUG, false)
         config.addDefault(VIDA, 100)
         config.addDefault(QUANTIDADE_DE_PEIXES, 30)
         config.options().copyDefaults(true)
@@ -24,16 +28,19 @@ class App: JavaPlugin() {
             config
         )
 
-        server.pluginManager.registerEvents(
-            LulaSpawnEvent(diContainer),
-            this
-        )
+        if (config.getBoolean(ATIVAR_PLUGIN)) {
+            server.pluginManager.registerEvents(
+                LulaSpawnEvent(diContainer),
+                this
+            )
 
-        server.pluginManager.registerEvents(
-            LulaMorteEvent(diContainer),
-            this
-        )
+            server.pluginManager.registerEvents(
+                LulaMorteEvent(diContainer),
+                this
+            )
+        }
 
         super.onEnable()
+
     }
 }
